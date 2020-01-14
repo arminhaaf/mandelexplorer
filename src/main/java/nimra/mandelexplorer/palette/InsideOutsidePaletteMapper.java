72d3cfc2ba60433003e84f90777049537b420ea0
armin_haaf <armin.haaf@mercatis.com>
2020-01-14 20:50:47 +0100

    - introduce Inside-Outside Palette Mapper

---
+++ src/main/java/nimra/mandelexplorer/palette/InsideOutsidePaletteMapper.java
@@ -0,0 +1,53 @@
+package nimra.mandelexplorer.palette;
+
+import nimra.mandelexplorer.PaletteMapper;
+import org.beryx.awt.color.ColorFactory;
+import org.json.JSONObject;
+
+import java.awt.Color;
+
+/**
+ * Created: 27.12.19   by: Armin Haaf
+ *
+ * @author Armin Haaf
+ */
+public class InsideOutsidePaletteMapper extends PaletteMapper {
+
+    private Color insideColor = Color.white;
+    private Color outsideColor = Color.black;
+
+    @Override
+    public int map(final int pIter, final double pLastR, final double pLastI, final double pDistanceR, final double pDistanceI) {
+        if (pIter != getMaxIterations()) {
+            return outsideColor.getRGB();
+        } else {
+            return insideColor.getRGB();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "In&Out";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Inside Outside Coloring";
+    }
+
+
+    @Override
+    protected void toJson(final JSONObject pJSONObject) {
+        super.toJson(pJSONObject);
+        pJSONObject.put("insideColor", toColorString(insideColor));
+        pJSONObject.put("outsideColor", toColorString(outsideColor));
+    }
+
+    @Override
+    public void fromJson(final JSONObject pJSONObject) {
+        super.fromJson(pJSONObject);
+        insideColor = ColorFactory.valueOf(pJSONObject.optString("insideColor", "white").trim());
+        outsideColor = ColorFactory.valueOf(pJSONObject.optString("outsideColor", "white").trim());
+    }
+
+}
