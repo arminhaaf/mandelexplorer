0a2f5cb650adb0bcb649de547b933f3bad86d0e3
armin_haaf <armin.haaf@mercatis.com>
2020-01-18 13:41:00 +0100

    - introduce FastDD Impl

---
+++ src/main/java/nimra/mandelexplorer/FastDDMandelImpl.java
@@ -0,0 +1,40 @@
+package nimra.mandelexplorer;
+
+import nimra.mandelexplorer.opencl.OpenCLDevice;
+
+/**
+ * Created: 18.01.20   by: Armin Haaf
+ *
+ * @author Armin Haaf
+ */
+public class FastDDMandelImpl implements MandelImpl {
+    // avx2 dd und opencl dd für nicht CPU
+
+    private final MandelImpl cpuImpl = new DDMandelNative(DDMandelNative.Algo.AVXDoubleDouble);
+    private final MandelImpl gpuImpl = new DDOpenCLMandelImpl();
+
+    @Override
+    public boolean supports(final ComputeDevice pDevice) {
+        if ( pDevice==ComputeDevice.CPU) {
+            return true;
+        }
+        if ( pDevice.getDeviceDescriptor() instanceof OpenCLDevice ) {
+            return ((OpenCLDevice)pDevice.getDeviceDescriptor()).getDeviceType() != OpenCLDevice.Type.CPU;
+        }
+        return true;
+    }
+
+    @Override
+    public void mandel(final ComputeDevice pComputeDevice, final MandelParams pParams, final MandelResult pMandelResult, final Tile pTile) {
+        if ( pComputeDevice==ComputeDevice.CPU) {
+            cpuImpl.mandel(pComputeDevice, pParams, pMandelResult, pTile);
+        } else {
+            gpuImpl.mandel(pComputeDevice, pParams, pMandelResult, pTile);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Fast DD";
+    }
+}
