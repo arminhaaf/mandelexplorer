85726ab8bd83edbc83b7e8c55c5d96280fb81923
armin_haaf <armin.haaf@mercatis.com>
2020-01-31 09:32:38 +0100

    - minor improvements

--- src/test/cuda/MandelStdOut.cu
+++ src/test/cuda/MandelStdOut.cu
@@ -8,39 +8,25 @@
 extern "C"
 __global__ void computeFloat(
       int *iters,
-      double4 area,
+      float4 area,
       int maxIterations,
       float sqrEscapeRadius
       ) {
 
-   const float x = area.x + X*area.z;
-   const float y = area.y + Y*area.w;
-   const float cr = x;
-   const float ci = y;
+   const float cr = area.x + X*area.z;
+   const float ci = area.y + Y*area.w;
 
-
-   const float escape = sqrEscapeRadius;
-
-   float zr = x;
-   float zi = y;
+   float zr = 0;
+   float zi = 0;
    float new_zr = 0.0f;
 
    int count = 0;
    for (; count<maxIterations; count++){
-        const float zrsqr = zr * zr;
-        const float zisqr = zi * zi;
-
-        if ( (zrsqr + zisqr) >= escape ) {
-            break;
-        }
-
-        new_zr = (zrsqr - zisqr) + cr;
+        new_zr = (zr * zr - zi * zi) + cr;
         zi = ((2.0f * zr) * zi) + ci;
         zr = new_zr;
 
-        //If in a periodic orbit, assume it is trapped
-        if (zr == 0.0 && zi == 0.0) {
-            count = maxIterations;
+        if ( (zr * zr + zi * zi) >= sqrEscapeRadius ) {
             break;
         }
    }
@@ -51,39 +37,37 @@ __global__ void computeFloat(
 
 
 int main() {
-    int tWidth = 160;
-    int tHeight = 80;
-    int tMaxIter = 100;
+    int tWidth = 5000;
+    int tHeight = 5000;
+    int tMaxIter = 1000;
 
-    double4 tArea = {-1.5,-1.0, 2.0/tWidth, 2.0/tHeight};
+    float4 tArea = {-1.5f,-1.0f, 2.0f/tWidth, 2.0f/tHeight};
 
     int *tIters = (int*)malloc(tWidth*tHeight*sizeof(int));
 
     int *cuIters;
 
     cudaMalloc((void **)&cuIters, sizeof(int)*tWidth*tHeight);
-
-
     cudaMemcpy(cuIters, tIters ,tHeight*tWidth*sizeof(int) ,cudaMemcpyHostToDevice);
 
     int tThreadsX = 8;
     int tThreadsY = 4;
 
-   dim3 blockSize = dim3( tThreadsX,tThreadsY);
-   dim3 gridSize  = dim3( tWidth/tThreadsX, tHeight/tThreadsY );
+    dim3 blockSize = dim3( tThreadsX,tThreadsY);
+    dim3 gridSize  = dim3( tWidth/tThreadsX, tHeight/tThreadsY );
 
     computeFloat<<<gridSize, blockSize>>>(
           cuIters,
           tArea,
           tMaxIter,
-          2);
-
-    cudaDeviceSynchronize();
+          4);
+     cudaDeviceSynchronize();
 
     cudaMemcpy(tIters, cuIters ,tHeight*tWidth*sizeof(int) ,cudaMemcpyDeviceToHost);
 
-    for ( int y=0;y<tHeight; y++) {
-        for ( int x=0; x<tWidth; x++) {
+    // 160 chars width and 80 chars height
+    for ( int y=0;y<tHeight; y+=tHeight/80) {
+        for ( int x=0; x<tWidth; x+=tWidth/160) {
             if ( tIters[x+y*tWidth]==tMaxIter) {
               printf("X");
             } else {
